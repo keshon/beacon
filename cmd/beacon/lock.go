7bd9a732d3cf33410cd9e3113234222d0ea7a641
Innokentiy Sokolov <keshon@zoho.com>
2026-06-29 17:25:47 +0300

    added missing files

---
+++ cmd/beacon/lock.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+func acquireDataDirLock(dataDir string) (*os.File, error) {
+	if err := os.MkdirAll(dataDir, 0o755); err != nil {
+		return nil, err
+	}
+	path := filepath.Join(dataDir, ".beacon.lock")
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0o644)
+	if err != nil {
+		return nil, err
+	}
+	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+		f.Close()
+		return nil, fmt.Errorf("data directory locked (is the Beacon server running?)")
+	}
+	return f, nil
+}
+
+func releaseDataDirLock(f *os.File) {
+	if f == nil {
+		return
+	}
+	_ = syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
+	_ = f.Close()
+}
