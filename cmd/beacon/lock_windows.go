f7ca5d1e7481fda17b151b41d83dfe0b8b594409
Innokentiy Sokolov <keshon@zoho.com>
2026-06-29 17:32:21 +0300

    fix locking

---
+++ cmd/beacon/lock_windows.go
@@ -0,0 +1,80 @@
+//go:build windows
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	modkernel32      = syscall.NewLazyDLL("kernel32.dll")
+	procLockFileEx   = modkernel32.NewProc("LockFileEx")
+	procUnlockFileEx = modkernel32.NewProc("UnlockFileEx")
+)
+
+const (
+	lockfileExclusiveLock   = 0x00000002
+	lockfileFailImmediately = 0x00000001
+)
+
+func acquireDataDirLock(dataDir string) (*os.File, error) {
+	if err := os.MkdirAll(dataDir, 0o755); err != nil {
+		return nil, err
+	}
+	path := filepath.Join(dataDir, ".beacon.lock")
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0o644)
+	if err != nil {
+		return nil, err
+	}
+	if err := lockFile(syscall.Handle(f.Fd())); err != nil {
+		f.Close()
+		return nil, fmt.Errorf("data directory locked (is the Beacon server running?)")
+	}
+	return f, nil
+}
+
+func releaseDataDirLock(f *os.File) {
+	if f == nil {
+		return
+	}
+	_ = unlockFile(syscall.Handle(f.Fd()))
+	_ = f.Close()
+}
+
+func lockFile(h syscall.Handle) error {
+	var ol syscall.Overlapped
+	r, _, err := procLockFileEx.Call(
+		uintptr(h),
+		uintptr(lockfileExclusiveLock|lockfileFailImmediately),
+		0,
+		1,
+		0,
+		uintptr(unsafe.Pointer(&ol)),
+	)
+	if r == 0 {
+		if err == syscall.Errno(0) {
+			return syscall.Errno(33) // ERROR_LOCK_VIOLATION
+		}
+		return err
+	}
+	return nil
+}
+
+func unlockFile(h syscall.Handle) error {
+	var ol syscall.Overlapped
+	r, _, err := procUnlockFileEx.Call(
+		uintptr(h),
+		0,
+		1,
+		0,
+		uintptr(unsafe.Pointer(&ol)),
+	)
+	if r == 0 {
+		return err
+	}
+	return nil
+}
